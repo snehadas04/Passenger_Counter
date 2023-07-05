8d04095b55b4c8381f88e6c9520bc72ca14a5a05
Sneha Das <89447948+snehadas04@users.noreply.github.com>
2023-07-06 02:13:15 +0530

    Add files via upload

--- Passenger Counter/script.js
+++ Passenger Counter/script.js
@@ -1,5 +1,7 @@
 // document.getElementById("count-el").innerText = 5
-
+let countEl = document.getElementById("count-el")
+let count = 0
 function increment(){
-    console.log("Button was Clicked")
+    count = count+1
+    countEl.innerText = count
 }
